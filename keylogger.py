cb587dcce7d4e6aa4db2bfbcda42692b389a2847
Aditya Taksande <taksandeaditya@gmail.com>
2019-12-31 17:03:24 +0530

    Update Keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -2,14 +2,4 @@ import pynput
 
 from pynput.keyboard import Key, Listener
 
-def onpress(key):
-    global keys, count
-    print("(0) pressed".format(key))
-
-def onrelease(key):
-    if key == Key.esc:
-        return False
-
-
-with Listener(onpress=onpress, onrelease=onrelease) as listener:
-    listener.join()
+with Listener(on_press=onpress,on_release=onrelease)
